0d4213277d412a9f3b2cab285f499aaf47aa8282
patunia <pborowiec@alumni.nd.edu>
2018-07-31 15:44:26 -0500

    structure of game

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Card from "./components/Card";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+// import Home from "./Pages/Home";
+import Jumbotron from "./components/Jumbotron";
+import NavBar from "./components/Navbar";
+import "./index.css";
+
+const App = () => (
+  <Router>
+    <div>
+      <NavBar />
+      <Jumbotron />
+        <Container />
+          <Card />
+          {/* <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+
+          </Switch> */}
+      <Footer />
+    </div>
+  </Router>
+);
 
 export default App;
+
+
+// came with build ->
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">mMmory Game Time</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
