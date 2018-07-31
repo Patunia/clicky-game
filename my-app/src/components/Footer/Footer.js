b5cc9f18462b9a20960ff8ba52b50cc6b1975a22
patunia <pborowiec@alumni.nd.edu>
2018-07-31 15:50:30 -0500

    more components

--- my-app/src/components/Footer/Footer.js
+++ my-app/src/components/Footer/Footer.js
@@ -3,8 +3,8 @@ import "./Footer.css";
 
 export default () => (
   <footer className="footer">
-    <div className=<"foot">
-      <p className="merge-conflict"> &copy; PJB and bootcamp</p>
+    <div className="foot">
+      <p> &copy; PJB and bootcamp</p>
     </div>
   </footer>
 );
