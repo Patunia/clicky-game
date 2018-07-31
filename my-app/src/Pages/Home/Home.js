93dd07831bda7635c0afff9f5280dc66d69f2701
patunia <pborowiec@alumni.nd.edu>
2018-07-31 16:42:23 -0500

    json and front page

--- my-app/src/Pages/Home/Home.js
+++ my-app/src/Pages/Home/Home.js
@@ -1,13 +1,58 @@
-import React from "react";
+import React, {Component} from "react";
+import Card from "../../components/Card";
+import turtle from "./../../../../my-app/src/turtle.json"
 import "./Home.css";
 
-const Home = () => (
-  <div className="container-fluid">
-    <div className="home-contain">
-      <h1> Click away!</h1>
-    </div>
-  </div>
+class Home extends Component {
 
-  );
+  state = {
+      turtles: [],
+      score: 0,
+      topScore: 0,
+      selectedturtle: [], 
+      navMessage: '',
+  };
+
+  componentDidMount() {
+      this.loadDefaultScore();
+  };
+
+  loadDefaultScore = () => {
+      this.setState({
+          turtles: turtle,
+          score: 0,
+          topScore: 0,
+          selectedturtle: [],
+          message: 'Click one image to start game!',
+      });
+  };
+
+  //need to add mixing function
+
+  render() {
+    return (
+        <container>
+        <row>
+        {this.state.turtles.map(turtle => {
+            return (<Card 
+                key = {turtle.id} 
+                id = {turtle.id} 
+                url = {turtle.url}
+                imgClickFunc = {this.imgClickFunc}
+              name ={turtle.name} /> )
+            })}
+          </row>
+        </container>
+    )
+};
+}
+// const Home = () => (
+//   <div className="container-fluid">
+//     <div className="home-contain">
+//       <h1> Click away!</h1>
+//     </div>
+//   </div>
+
+//   );
 
   export default Home;
--- my-app/src/components/NavBar/NavBar.js
+++ my-app/src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
-import React from "react";
-// import { Link } from "react-router-dom";
-import "./NavBar.css";
+// import React from "react";
+// // import { Link } from "react-router-dom";
+// import "./NavBar.css";
 
 // const DisplayChoice = (props) => {
 //   console.log(props)
